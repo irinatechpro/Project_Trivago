8f7c9bd075eccb16cb143810c48d6a61c174d685
Arlisa-Dem <arlisademiraj@gmail.com>
2023-12-26 23:59:20 +0000

    Commits from Arlisa

---
+++ src/test/java/project/pages/HotelPage.java
@@ -0,0 +1,26 @@
+package project.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HotelPage  extends BasePage{
+
+    @FindBy(xpath = "(//button[@data-testid='item-name'])[1]")
+    public WebElement randomHotel;
+
+    @FindBy(xpath = "(//span[@class='inline-flex leading-none transform stroke-current flex-shrink-0 ml-3'])[2]")
+    public WebElement showAllPrices;
+
+    @FindBy(xpath = "(//button[@type='button'])[26]")
+    public WebElement infoAmenities;
+
+    @FindBy(xpath = "(//button[@class='bg-white text-s font-bold px-3 leading-none h-8 border rounded-xs border-grey-700 hover:text-white hover:bg-grey-700'])[3]")
+    public WebElement showAmenities;
+
+
+    @FindBy(xpath = "(//span[@data-testid='popular-badge'])[1]")
+    public WebElement popularChoice;
+
+
+
+}
