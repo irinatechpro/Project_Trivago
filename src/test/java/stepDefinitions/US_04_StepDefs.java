33e82753667515a102c20db4a76644ebcdbba9a4
Arlisa-Dem <arlisademiraj@gmail.com>
2023-12-26 17:12:41 +0000

    Commits from Arlisa

---
+++ src/test/java/stepDefinitions/US_04_StepDefs.java
@@ -0,0 +1,72 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class US_04_StepDefs {
+
+
+    @When("Choose a place as destination and select all the necessary steps than click on Search")
+    public void choose_a_place_as_destination_and_select_all_the_necessary_steps_than_click_on_search() {
+
+    }
+
+    @Then("Click on a hotel from the search results")
+    public void click_on_a_hotel_from_the_search_results() {
+
+    }
+
+    @Then("Check if the details include accurate pricing")
+    public void check_if_the_details_include_accurate_pricing() {
+
+    }
+
+    @Then("Check if the details include accurate amenities")
+    public void check_if_the_details_include_accurate_amenities() {
+
+    }
+
+    @Then("Verify that buttons and links on the page are functional")
+    public void verify_that_buttons_and_links_on_the_page_are_functional() {
+
+    }
+
+    /*
+    -------------------------------------------------------------------------------------------
+     */
+
+
+
+    @When("Choose a place as destination and click on Search")
+    public void choose_a_place_as_destination_and_click_on_search() {
+
+    }
+
+    @Then("Click on a hotel from the search results page")
+    public void click_on_a_hotel_from_the_search_results_page() {
+
+    }
+
+    @Then("Click on View Deal button")
+    public void click_on_view_deal_button() {
+
+    }
+
+    @Then("Click on See description")
+    public void click_on_see_description() {
+
+    }
+
+    @Then("Click on See all services")
+    public void click_on_see_all_services() {
+
+    }
+
+    @Then("Click on See the rates and Pricing conditions")
+    public void click_on_see_the_rates_and_pricing_conditions() {
+
+    }
+
+
+
+
+}
